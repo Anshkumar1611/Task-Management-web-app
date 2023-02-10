9c2eb0dc2fd799d3fe03202d751bb0d2107c640d
Ansh Kumar <ansh2018gupta@gmail.com>
2023-02-10 12:30:41 +0530

    add task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,7 +3,7 @@ import { useStateContext } from '../context/ContextProvider'
 import Button from "./Button"
 
 const AddTask = () => {
-  const { addTask, text, setText, day, setDay, priority, setPriority } = useStateContext()
+  const { addTask, text, setText, day, setDay } = useStateContext()
 
   return (
     <section className='flex justify-center items-center '>
@@ -25,7 +25,7 @@ const AddTask = () => {
             <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-violet-400 " placeholder="day & time" name="day" value={day} onChange={(e) => setDay(e.target.value)} />
           </div>
         </div>
-        <div className='md:flex md:items-center mb-6'>
+        {/* <div className='md:flex md:items-center mb-6'>
           <div className="md:w-1/3">
             <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4" for="grid-state">
               Priority
@@ -41,7 +41,7 @@ const AddTask = () => {
               <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className='flex justify-end'>
           <Button text={"Add Task"} />
         </div>
